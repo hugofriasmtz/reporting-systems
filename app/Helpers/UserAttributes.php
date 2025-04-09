b93a5197bd57bd11f15a5311301df1d25d523950
hugofriasmtz <a3521110393@alumno.uttehuacan.edu.mx>
2025-04-09 11:43:36 -0600

    :sparkles: Introduce new features.

--- app/Helpers/UserAttributes.php
+++ app/Helpers/UserAttributes.php
@@ -5,17 +5,33 @@ namespace App\Helpers;
 
 class UserAttributes
 {
-    public function GetCurrentDateTime(){
-        $date = new \DateTimeZone('America/Mexico_City');
-        return $date = format('Y-m-d H:i:s');
+    public static function FormatDate($date, $format = 'Y-m-d H:i:s', $timezone = 'America/Mexico_City')
+    {
+        try {
+            // Crear un objeto DateTime con la fecha proporcionada y la zona horaria especificada
+            $dateTime = new \DateTime($date, new \DateTimeZone($timezone));
+            
+            // Devolver la fecha en el formato deseado
+            return $dateTime->format($format);
+        } catch (\Exception $e) {
+            // Manejar errores y devolver un mensaje
+            return 'Error al formatear la fecha: ' . $e->getMessage();
+        }
     }
+
 
     public static function GetStatusDetails($status)
     {
         switch ($status) {
-            case 'ACTIVE':   return ['label' => 'Activo', 'class' => 'success']; 
-            case 'INACTIVE': return ['label' => 'Suspendido', 'class' => 'warning'];
-            case 'DELETED':  return ['label' => 'Eliminado', 'class' => 'danger'];
+            case 'ACTIVE':      return ['label' => 'Activo', 'class' => 'success']; 
+            case 'INACTIVE':    return ['label' => 'Suspendido', 'class' => 'warning'];
+            case 'DELETED':     return ['label' => 'Eliminado', 'class' => 'danger'];
+            case 'PENDING':     return ['label' => 'Pendiente', 'class' => 'info', 'color' => 'primary', 'porcentage' => '0%'];
+            case 'IN_PROGRESS': return ['label' => 'En Progreso', 'class' => 'dark','color' => 'dark', 'porcentage' => '50%'];
+            case 'RESOLVED':    return ['label' => 'Resuelto', 'class' => 'success', 'color' => 'success', 'porcentage' => '100%'];
+            case 'LOW':         return ['label' => 'Baja', 'class' => 'success'];
+            case 'MEDIUM':      return ['label' => 'Media', 'class' => 'warning'];
+            case 'HIGH':        return ['label' => 'Alta', 'class' => 'danger'];
         }
     }
     
@@ -37,6 +53,24 @@ class UserAttributes
         
     }
 
+    public static function GetIssuetypesDetails($rol)
+    {
+        switch ($rol) {
+            case 'CLEANING':    return 'Limpieza';      break;
+            case 'MAINTENANCE': return 'Mantenimiento'; break;
+            case 'SYSTEM':      return 'Sistemas';      break;
+            default:            return 'Otros';         break;
+        }
+    }
+    public static function GetDetails($rol)
+    {
+        switch ($rol) {
+            case 'ADMINISTRATOR':      return 'Administrador';    break;
+            case 'PARTNER_IN_CHARGE':  return 'Encargado';        break;
+            case 'COLLABORATOR':       return 'Colaborador';      break;
+            default:                   return 'Sin rol asignado'; break;
+        }
+    }
     public static function getRolDetails($rol)
     {
         return $rol === 'Partner_in_charge' ? 'Encargado' : 'Colaboradores';
